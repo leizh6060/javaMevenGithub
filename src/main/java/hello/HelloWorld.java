f65dcba02a9be91d66eca57f9a2c1e09e3aab01d
leizh6060 <leizh6060@yahoo.com>
2026-06-24 15:47:18 -0400

    create bug in helloworld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -25,6 +25,6 @@ public class HelloWorld {
     LocalTime currentTime = new LocalTime();
     logger.info("The current local time is: " + currentTime);
     Greeter greeter = new Greeter();
-    logger.info(greeter.sayHello());
+    logger.info(greeter.sayHello( Lei));
   }
 }
